d18eadab18a891f58b38b9d782d83735583159fa
Prajval <prajvalhl@gmail.com>
2021-10-27 20:15:08 +0530

    refactor: added support for mobile devices

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-window.addEventListener("keydown", (e) => {
+window.addEventListener("keydown", play);
+
+function play(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  console.log(audio);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
@@ -13,4 +16,20 @@ function removeTransition(e) {
 }
 
 const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) =>
+  key.addEventListener("click", () => {
+    const audio = document.querySelector(
+      `audio[data-key='${key.dataset.key}']`
+    );
+    const alpkey = document.querySelector(
+      `.key[data-key='${key.dataset.key}']`
+    );
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    alpkey.classList.add("playing");
+  })
+);
+
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
